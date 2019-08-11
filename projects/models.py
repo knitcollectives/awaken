31f13fcf87409b8484049c3743d2734b7966b524
Steven Thomas <pro.steven.thomas@gmail.com>
2019-08-11 15:09:31 -0400

    updated Project, added ProjectAdmin model

--- projects/models.py
+++ projects/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from datetiem import datetime
 
 # Create your models here.
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     category = models.CharField(max_length=20)
+    published = models.DateTimeField("date published", default=datetime.now())
     image = models.FilePathField(path="/img")
