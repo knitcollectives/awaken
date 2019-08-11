2d666459d6650f44b0915ebe81312f1a549b04a3
Steven Thomas <pro.steven.thomas@gmail.com>
2019-08-11 17:58:59 -0400

    adding ProjectAdmin model to admin panel

--- projects/admin.py
+++ projects/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.db import models
-from .models import Project
+from project.models import Project
 
 
 # Register your models here.
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Project, ProjectAdmin)
