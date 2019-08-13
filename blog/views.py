a1176b9cd62edd6202dbf9e47b3e1959fa1c7f4c
Steven Thomas <pro.steven.thomas@gmail.com>
2019-08-12 21:59:50 -0400

    added templates to blog app, updated nav bar link to blogs

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from blog.models import Post
+from blog.models import Post, Comment
+from blog.forms import CommentForm
 
 # Create your views here.
 def blog_index(request):
@@ -11,19 +12,34 @@ def blog_index(request):
 
 
 def blog_category(request, category):
-    posts = Post.objects.filter(categories__name__contains=category).order_by('-created_on')
+    posts = Post.objects.filter(categories__name__contains=category).order_by(
+        '-created_on'
+    )
     context = {
         "category": category,
-        "posts": posts
+        "posts": posts,
     }
     return render(request, "blog_category.html", context)
 
-    def blog_detail(request, pk):
+
+def blog_detail(request, pk):
     post = Post.objects.get(pk=pk)
+
+    form = CommentForm()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+                author=form.cleaned_data["author"],
+                body=form.cleaned_data["body"],
+                post=post,
+            )
+            comment.save()
+
     comments = Comment.objects.filter(post=post)
     context = {
         "post": post,
         "comments": comments,
+        "form":form,
     }
-
     return render(request, "blog_detail.html", context)
